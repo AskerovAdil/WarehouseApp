640d6247019385fcfbd97c38a0e576529efcf215
AskerovAdil <Askerov121099@mail.ru>
2023-06-25 14:20:42 +0700

    feat: add file-storage manager

---
+++ StorageControl/Manager/StorageFileManager.cs
@@ -0,0 +1,60 @@
+﻿using WarehouseApp.Abstract;
+using WarehouseApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApp
+{
+    public class StorageFileManager
+    {
+        public Storage LoadFromFile(string filePath)
+        {
+            var storage = new Storage();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    var palletId = int.Parse(values[0]);
+                    var boxId = int.Parse(values[1]);
+                    var width = double.Parse(values[2]);
+                    var height = double.Parse(values[3]);
+                    var depth = double.Parse(values[4]);
+                    var weight = double.Parse(values[5]);
+                    var productionDate = DateTime.Parse(values[6]);
+
+                    var pallet = new Pallet(palletId, 100, 100, 100);
+                    storage.AddPalletIfNotExists(pallet);
+
+                    var box = new Box(boxId, width, height, depth, weight, productionDate);
+                    pallet.AddBox(box);
+                }
+            }
+
+            return storage;
+        }
+
+        public void SaveToFile(Storage storage, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            {
+                foreach (var pallet in storage.GetPallets())
+                {
+                    foreach (var box in pallet.GetBoxes())
+                    {
+                        var line = $"{pallet.Id},{box.Id},{box.Width},{box.Height},{box.Depth},{box.Weight},{box.ProductionDate.ToShortDateString()}";
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ StorageControl/Program.cs
@@ -0,0 +1,94 @@
+﻿using WarehouseApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseApp.Abstract;
+
+namespace WarehouseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var storage = new Storage();
+
+            var pallet1 = new Pallet(1, 100, 100, 100);
+            pallet1.AddBox(new Box(1, 50, 50, 50, 10, DateTime.Now));
+            pallet1.AddBox(new Box(2, 50, 50, 50, 20, DateTime.Now));
+            pallet1.AddBox(new Box(3, 50, 50, 50, 30, DateTime.Now));
+            storage.AddPalletIfNotExists(pallet1);
+
+            var pallet2 = new Pallet(2, 100, 100, 100);
+            pallet2.AddBox(new Box(4, 50, 50, 50, 10, new DateTime(2023, 2, 10)));
+            pallet2.AddBox(new Box(5, 50, 50, 50, 20, new DateTime(2023, 3, 01)));
+            pallet2.AddBox(new Box(6, 50, 50, 50, 30, DateTime.Now));
+            storage.AddPalletIfNotExists(pallet2);
+
+            var pallet3 = new Pallet(3, 100, 100, 100);
+            pallet3.AddBox(new Box(7, 50, 50, 50, 10, new DateTime(2020, 2, 1)));
+            pallet3.AddBox(new Box(8, 50, 50, 50, 20, DateTime.Now));
+            pallet3.AddBox(new Box(9, 50, 50, 50, 30, DateTime.Now));
+            storage.AddPalletIfNotExists(pallet3);
+
+            var pallet4 = new Pallet(3, 100, 100, 100);
+            pallet4.AddBox(new Box(7, 50, 50, 50, 10, DateTime.Now));
+            pallet4.AddBox(new Box(8, 50, 50, 50, 20, DateTime.Now));
+            pallet4.AddBox(new Box(9, 50, 50, 50, 30, DateTime.Now));
+            storage.AddPalletIfNotExists(pallet4);
+
+            storage.PrintPalletsByExpirationDate();
+            storage.PrintPalletsWithLongestExpirationDate(3);
+
+            Console.ReadKey();
+
+            var storageFileManager = new StorageFileManager();
+            storageFileManager.SaveToFile(storage, "data.csv");
+            storage = new Storage();
+            storage.PrintPalletsByExpirationDate();
+            storage.PrintPalletsWithLongestExpirationDate(3);
+
+            Console.ReadKey();
+
+            storage = storageFileManager.LoadFromFile("data.csv");
+            storage.PrintPalletsByExpirationDate();
+            storage.PrintPalletsWithLongestExpirationDate(3);
+
+
+            /*
+
+                        var storage = new Storage();
+
+                        var pallet1 = new Pallet(1, 100, 100, 100);
+                        pallet1.AddBox(new Box(1, 50, 50, 50, 10, DateTime.Now));
+                        pallet1.AddBox(new Box(2, 50, 50, 50, 20, DateTime.Now));
+                        pallet1.AddBox(new Box(3, 50, 50, 50, 30, DateTime.Now));
+                        storage.AddPalletIfNotExists(pallet1);
+
+                        var pallet2 = new Pallet(2, 100, 100, 100);
+                        pallet2.AddBox(new Box(4, 50, 50, 50, 10, new DateTime(2023, 2,10)));
+                        pallet2.AddBox(new Box(5, 50, 50, 50, 20, new DateTime(2023, 3, 01)));
+                        pallet2.AddBox(new Box(6, 50, 50, 50, 30, DateTime.Now));
+                        storage.AddPalletIfNotExists(pallet2);
+
+                        var pallet3 = new Pallet(3, 100, 100, 100);
+                        pallet3.AddBox(new Box(7, 50, 50, 50, 10, new DateTime(2020, 2, 1)));
+                        pallet3.AddBox(new Box(8, 50, 50, 50, 20, DateTime.Now));
+                        pallet3.AddBox(new Box(9, 50, 50, 50, 30, DateTime.Now));
+                        storage.AddPalletIfNotExists(pallet3);
+
+                        var pallet4 = new Pallet(3, 100, 100, 100);
+                        pallet4.AddBox(new Box(7, 50, 50, 50, 10, DateTime.Now));
+                        pallet4.AddBox(new Box(8, 50, 50, 50, 20, DateTime.Now));
+                        pallet4.AddBox(new Box(9, 50, 50, 50, 30, DateTime.Now));
+                        storage.AddPalletIfNotExists(pallet4);
+
+                        storage.PrintPalletsByExpirationDate();
+                        storage.PrintPalletsWithLongestExpirationDate(3);*/
+
+
+        }
+    }
+}
